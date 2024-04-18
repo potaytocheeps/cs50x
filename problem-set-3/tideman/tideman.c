26ba7654cf7723198535a4207bf382d3368df164
Potayto <83526209+PotaytoDev@users.noreply.github.com>
2024-04-17 19:42:17 -0700

    Update add_pairs() in Tideman problem of Week 3 problem set

--- problem-set-3/tideman/tideman.c
+++ problem-set-3/tideman/tideman.c
@@ -144,7 +144,28 @@ void record_preferences(int ranks[])
 // Record pairs of candidates where one is preferred over the other
 void add_pairs(void)
 {
-    // TODO
+    // Compare each candidate with every other candidate in preferences[]
+    for (int i = 0; i < candidate_count - 1; i++)
+    {
+        for (int j = i + 1; j < candidate_count; j++)
+        {
+            // Compare the number of times candidate i was preferred over
+            // candidate j and vice versa
+            if (preferences[i][j] > preferences[j][i])
+            {
+                pairs[pair_count].winner = i;
+                pairs[pair_count].loser = j;
+                pair_count++;
+            }
+            else if (preferences[i][j] < preferences[j][i])
+            {
+                pairs[pair_count].winner = j;
+                pairs[pair_count].loser = i;
+                pair_count++;
+            }
+        }
+    }
+
     return;
 }
 
