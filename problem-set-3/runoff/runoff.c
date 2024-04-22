c81abe52f5190367303bd742aefc2a2270cd44d7
Potayto <83526209+PotaytoDev@users.noreply.github.com>
2024-04-22 12:47:25 -0700

    Update print_winner() in Runoff problem

--- problem-set-3/runoff/runoff.c
+++ problem-set-3/runoff/runoff.c
@@ -172,7 +172,18 @@ void tabulate(void)
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
-    // TODO
+    // Check if any candidate has a vote count that is the majority of the votes
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes > (voter_count / 2))
+        {
+            printf("%s\n", candidates[i].name);
+
+            // A candidate has the vote majority
+            return true;
+        }
+    }
+
     return false;
 }
 
