c2eff560807acc56c7f8498a495b4cf03b223b66
Potayto <83526209+PotaytoDev@users.noreply.github.com>
2024-04-22 13:02:07 -0700

    Update find_min() in Runoff problem

--- problem-set-3/runoff/runoff.c
+++ problem-set-3/runoff/runoff.c
@@ -190,8 +190,19 @@ bool print_winner(void)
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
-    // TODO
-    return 0;
+    // Hold the minimum number of votes
+    int min = MAX_VOTERS + 1;
+
+    // Find the minimmum number of votes any candidate has
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes < min)
+        {
+            min = candidates[i].votes;
+        }
+    }
+
+    return min;
 }
 
 // Return true if the election is tied between all candidates, false otherwise
