b24b76c757f83d170bb3615a8abb8715552c64df
Potayto <83526209+PotaytoDev@users.noreply.github.com>
2024-06-06 12:56:17 -0700

    Implement grayscale filter in Filter (less) problem

--- problem-set-4/filter-less/helpers.c
+++ problem-set-4/filter-less/helpers.c
@@ -1,8 +1,33 @@
+#include <math.h>
+
 #include "helpers.h"
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Iterate over every pixel
+    for (int row = 0; row < height; row++)
+    {
+        for (int column = 0; column < width; column++)
+        {
+            // Get values of each color
+            int blue = image[row][column].rgbtBlue;
+            int green = image[row][column].rgbtGreen;
+            int red = image[row][column].rgbtRed;
+
+            // Get the average of blue, green, and red
+            float average = blue + green + red;
+            average /= 3.0;
+
+            // Round to nearest integer
+            int gray = (int) round(average);
+
+            // Set each pixel's colors to the updated grayscale color
+            image[row][column].rgbtBlue = gray;
+            image[row][column].rgbtGreen = gray;
+            image[row][column].rgbtRed = gray;
+        }
+    }
     return;
 }
 
