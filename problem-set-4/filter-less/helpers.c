994e7f00f11b9013eb28b3581004b5c915397b3d
Potayto <83526209+PotaytoDev@users.noreply.github.com>
2024-06-12 09:08:27 -0700

    Implement blur filter in Filter (less) problem

--- problem-set-4/filter-less/helpers.c
+++ problem-set-4/filter-less/helpers.c
@@ -98,5 +98,52 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Make a copy of the image
+    RGBTRIPLE copy[height][width];
+
+    for (int row = 0; row < height; row++)
+    {
+        for (int column = 0; column < width; column++)
+        {
+            copy[row][column] = image[row][column];
+        }
+    }
+
+    // Iterate over every pixel
+    for (int row = 0; row < height; row++)
+    {
+        for (int column = 0; column < width; column++)
+        {
+            // Declare variables to hold the total sum of each color neighboring the current pixel
+            float redSum = 0;
+            float blueSum = 0;
+            float greenSum = 0;
+            float pixelSum = 0;
+
+            // Iterate over the pixels neighboring the current pixel, including the current pixel
+            for (int i = row - 1; i <= row + 1; i++)
+            {
+                for (int j = column - 1; j <= column + 1; j++)
+                {
+                    // Make sure the indexes are positive and that they don't go out of range
+                    if ((i >= 0 && j >= 0) && (i < height && j < width))
+                    {
+                        // Calculate the sum of each color value for every pixel in a square shape
+                        redSum += copy[i][j].rgbtRed;
+                        greenSum += copy[i][j].rgbtGreen;
+                        blueSum += copy[i][j].rgbtBlue;
+                        pixelSum++;
+                    }
+                }
+            }
+
+            // Update the image's pixels with the average of the color values for each pixel
+            // neighboring the current pixel
+            image[row][column].rgbtRed = (int) round(redSum / pixelSum);
+            image[row][column].rgbtGreen = (int) round(greenSum / pixelSum);
+            image[row][column].rgbtBlue = (int) round(blueSum / pixelSum);
+        }
+    }
+
     return;
 }
