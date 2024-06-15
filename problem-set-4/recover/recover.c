6d4d7aef4ecee7cd5b6f5a38c9bd5c2256b89bfe
Potayto <83526209+PotaytoDev@users.noreply.github.com>
2024-06-14 19:04:10 -0700

    Complete implementation of Recover problem

--- problem-set-4/recover/recover.c
+++ problem-set-4/recover/recover.c
@@ -8,6 +8,7 @@
 typedef uint8_t BYTE;
 // The forensic image is in FAT file format, so each block of memory is 512 bytes
 const int BLOCK_SIZE = 512;
+const int FILENAME_SIZE = 8;
 
 int main(int argc, char *argv[])
 {
@@ -19,43 +20,73 @@ int main(int argc, char *argv[])
     }
 
     // Store input file name
-    char *input_filename = argv[1];
+    char *in_filename = argv[1];
 
     // Open the file given as an argument for reading
-    FILE *input_file = fopen(input_filename, "r");
+    FILE *input_file = fopen(in_filename, "r");
     if (input_file == NULL)
     {
-        printf("%s could not be opened.\n", input_filename);
+        printf("%s could not be opened.\n", in_filename);
         return 1;
     }
 
     // Create a buffer to store the information from the memory card
     BYTE buffer[BLOCK_SIZE];
 
-    // Read information from the input file into the buffer
+    // Declare counter for number of jpegs found to be used in output file names
     int jpegs_found = 0;
 
+    // Allocate memory for the name of the output file.
+    char *out_filename = malloc(sizeof(char) * FILENAME_SIZE);
+
+    // Declare file pointer for output file
+    FILE *output_file = NULL;
+
+    // Read information from the input file into the buffer
     while (fread(buffer, sizeof(BYTE), BLOCK_SIZE, input_file) != 0)
     {
         // Check the first four bytes in buffer to determine whether it is a JPEG
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff &&
             ((buffer[3] & 0xf0) == 0xe0))
         {
-            // IF it is a JPEG
-            // IF this is first JPEG file found, open a new file for writing information into
-            // ELSE, close the currently opened output file and open a new one for writing
-            jpegs_found++;
-        }
+            // If a jpeg file had previously been found, close that file
+            if (output_file)
+            {
+                fclose(output_file);
+            }
 
-        // ELSE
-            // IF already found JPEG, continue writing to it
-                // I can check if a JPEG has been found if an output file exists. If it doesn't,
-                // then no JPEG file has yet been found and the program can continue reading
-                // information from the input file
+            // Name the JPEG file
+            sprintf(out_filename, "%03i.jpg", jpegs_found);
+
+            // Open output file for writing
+            output_file = fopen(out_filename, "w");
+            if (output_file == NULL)
+            {
+                fclose(input_file);
+                printf("%s could not be created.\n", out_filename);
+                return 1;
+            }
+
+            // Increase the jpeg counter
+            jpegs_found++;
+
+            // Write information in buffer into output file
+            fwrite(buffer, sizeof(BYTE), BLOCK_SIZE, output_file);
+        }
+        else
+        {
+            // If already found JPEG, continue writing to it
+            if (output_file)
+            {
+                fwrite(buffer, sizeof(BYTE), BLOCK_SIZE, output_file);
+            }
+        }
     }
+
+    // Free memory used for output file name
+    free(out_filename);
 
     // Close files
     fclose(input_file);
-
-    printf("Found %i images\n", jpegs_found);
+    fclose(output_file);
 }
