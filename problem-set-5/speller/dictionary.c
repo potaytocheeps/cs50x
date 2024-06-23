314df2ff21702a7486f3a2f7d0b6104cb99728f2
Potayto <83526209+PotaytoDev@users.noreply.github.com>
2024-06-23 13:28:52 -0700

    Improve speed of Speller program
    Updated check(), hash(), and load() to improve the speed at which the
    program stores words into the hash table and checks if a word is in
    the hash table.
    
      - I created a new hash function which now determines a hash value
        based on the first letter of the word and the number of letters in
        the word.
    
      - The size of the hash table was increased to 4 times its previous
        size, as each letter in the alphabet will now have four buckets
        to separate words based on their length.
    
      - New nodes are now added at the end of each list as opposed to
        the beginning of the list, which keeps the lists alphabetized.
    
      - Checking whether a word is in the hash table is now slightly faster
        as the program will now search only half of the linked list based
        on whether the word should be located after or before the word
        at the middle of the list.

--- problem-set-5/speller/dictionary.c
+++ problem-set-5/speller/dictionary.c
@@ -16,8 +16,8 @@ typedef struct node
     struct node *next;
 } node;
 
-// TODO: Choose number of buckets in hash table
-const unsigned int N = 26;
+// Number of buckets in hash table: four per letter of alphabet
+const unsigned int N = 104;
 
 // Track size of dictionary
 int dictionary_size = 0;
@@ -25,20 +25,63 @@ int dictionary_size = 0;
 // Hash table
 node *table[N];
 
+// Track middle of linked lists
+node *middle[N];
+
+// Track tail of linked lists
+node *tail[N];
+
+// Track whether a linked list has an even number of nodes
+bool list_is_even[N];
+
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
 {
     // Call hash function on word to get a hash value
     unsigned int index = hash(word);
 
+    // Guard against accessing a field of a NULL pointer
+    if (middle[index] == NULL)
+    {
+        return false;
+    }
+
+    // Declare pointers to iterate linked lists
+    node *current_node = NULL;
+    node *end = NULL;
+
+    // Word is located at the middle of the linked list
+    if (strcasecmp(word, middle[index]->word) == 0)
+    {
+        return true;
+    }
+    // Word is located in the first half of the linked list
+    else if (strcasecmp(word, middle[index]->word) < 0)
+    {
+        // Start iterating from beginning of list
+        current_node = table[index];
+
+        // Only check the first half of the list
+        end = middle[index];
+    }
+    // Word is located in the second half of the linked list
+    else
+    {
+        // Start iterating from middle of list
+        current_node = middle[index];
+    }
+
     // Traverse the linked list located at the hash value in the array
-    for (node *list = table[index]; list != NULL; list = list->next)
+    while (current_node != end)
     {
         // Compare each node to the word to see if it is located in the dictionary
-        if (strcasecmp(list->word, word) == 0)
+        if (strcasecmp(word, current_node->word) == 0)
         {
             return true;
         }
+
+        // Continue with next node in linked list
+        current_node = current_node->next;
     }
 
     return false;
@@ -47,8 +90,35 @@ bool check(const char *word)
 // Hashes word to a number
 unsigned int hash(const char *word)
 {
-    // TODO: Improve this hash function
-    return toupper(word[0]) - 'A';
+    // Get index starting from 0 of first letter in word
+    unsigned int index = toupper(word[0]) - 'A';
+
+    // There are four buckets per letter
+    index *= 4;
+
+    // Get length of word
+    int word_length = strlen(word);
+
+    // First bucket has words of length 1-4
+    if (word_length > 0 && word_length <= 4)
+    {
+        return index;
+    }
+    // Second bucket has words of length 5-8
+    else if (word_length > 4 && word_length <= 8)
+    {
+        return index + 1;
+    }
+    // Third bucket has words of length 9-12
+    else if (word_length > 8 && word_length <= 12)
+    {
+        return index + 2;
+    }
+    // Fourth bucket has words of length 13-45
+    else
+    {
+        return index + 3;
+    }
 }
 
 // Loads dictionary into memory, returning true if successful, else false
@@ -58,6 +128,9 @@ bool load(const char *dictionary)
     for (int i = 0; i < N; i++)
     {
         table[i] = NULL;
+        middle[i] = NULL;
+        tail[i] = NULL;
+        list_is_even[i] = false;
     }
 
     // Open dictionary file
@@ -83,6 +156,7 @@ bool load(const char *dictionary)
             printf("Could not allocate memory for new node.\n");
             return false;
         }
+        new_node->next = NULL;
 
         // Copy word into node
         strcpy(new_node->word, word);
@@ -90,9 +164,37 @@ bool load(const char *dictionary)
         // Hash word to obtain a hash value
         unsigned int index = hash(word);
 
-        // Add new node into linked list
-        new_node->next = table[index];
-        table[index] = new_node;
+        // If this is the first node in the list
+        if (table[index] == NULL)
+        {
+            // Add new node into linked list and set all pointers to it
+            table[index] = new_node;
+            middle[index] = new_node;
+            tail[index] = new_node;
+        }
+        // If list has an even number of nodes
+        else if (list_is_even[index])
+        {
+            // Add new node to the end of the list
+            tail[index]->next = new_node;
+            tail[index] = new_node;
+
+            // List now has an odd number of nodes
+            list_is_even[index] = false;
+        }
+        // If list has an odd number of nodes
+        else
+        {
+            // Add new node to the end of the list
+            tail[index]->next = new_node;
+            tail[index] = new_node;
+
+            // Update the middle of the list
+            middle[index] = middle[index]->next;
+
+            // List now has an even number of nodes
+            list_is_even[index] = true;
+        }
 
         // Increase dictionary size counter
         dictionary_size++;
