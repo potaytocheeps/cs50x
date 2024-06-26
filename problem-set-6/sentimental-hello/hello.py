460831464bb15cdac905d47643bf0d423cb3e010
Potayto <83526209+PotaytoDev@users.noreply.github.com>
2024-06-26 12:30:35 -0700

    Complete implementation of Hello problem of Week 6 problem set

---
+++ problem-set-6/sentimental-hello/hello.py
@@ -0,0 +1,9 @@
+# Greets users by their name
+
+from cs50 import get_string
+
+# Ask user for their name
+name = get_string("What is your name? ")
+
+# Print message to greet user
+print(f"Hello, {name}")
